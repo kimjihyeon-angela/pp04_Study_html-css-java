85294a0db797ee0400863a48aa92ac163840814f
kjh <angela9830@naver.com>
2023-10-30 11:02:10 +0900

    2023.10.30.

---
+++ Class/Day04/10_variable.js
@@ -0,0 +1,17 @@
+// console.log("콘솔로그를 찍어봅니다")
+let name
+
+name = "홍길동"
+
+name = "홍길순"
+
+console.log(name)
+
+// name에는 가장 마지막에 넣어준 홍길순이 들어가있음
+
+const name2 = "홍길동"
+
+// name2 = "스티브 잡스"
+// 상수에 재할당 불가능하기 때문에 오류 새김
+
+console.log(name2)
