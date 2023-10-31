b2fcc0bfa2180bbac5b9f5a7686990ba4bf820da
kjh <angela9830@naver.com>
2023-10-31 13:34:11 +0900

    2023.10.31.

---
+++ Class/Day05/17_variable_if.js
@@ -0,0 +1,30 @@
+//  데이터 타입, 연산자 연습
+1 + 1
+// 2
+1 + "만원"
+// '1만원'
+1 + "1"
+// '11'
+1 - "1"
+// 0
+"코드" + "캠프"
+// '코드캠프'
+"123" == 123
+// true
+"123" === 123
+// false
+true && true
+// true
+true && false
+// false
+true || true
+// true
+true && false
+// false
+true || false
+// true
+!false
+// true
+!true
+// false
+
