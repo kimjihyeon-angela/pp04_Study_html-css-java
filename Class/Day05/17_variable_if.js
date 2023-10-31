57861eeaf95ea2d94e04523ee6cc1cd9366d8015
kjh <angela9830@naver.com>
2023-10-31 13:52:23 +0900

    2023.10.31.

--- Class/Day05/17_variable_if.js
+++ Class/Day05/17_variable_if.js
@@ -28,3 +28,73 @@ true || false
 !true
 // false
 
+// 조건문
+if(1 + 1 === 2){
+    console.log("정답입니다.")
+    }
+    else{
+        console.log("틀렸습니다.")
+    }
+    // VM328:2 정답입니다.
+    undefined
+    if(true){
+    console.log("정답입니다.")
+    }
+    else{
+        console.log("틀렸습니다.")
+    }
+    // VM341:2 정답입니다.
+    undefined
+    if(!true){
+        console.log("정답입니다.")
+    }
+    else{
+        console.log("틀렸습니다.")
+    }
+    // VM364:5 틀렸습니다.
+    undefined
+    if(0){
+        console.log("정답입니다.")
+    }
+    else{
+        console.log("틀렸습니다.")
+    }
+    // VM372:5 틀렸습니다.
+    //  0 => 거짓같은 값으로 false가 나옴
+
+    const profile = {
+        name : "철수",
+        age : 12,
+        school:"다람쥐초등학교"
+    }
+    // undefined
+    if (profile.age >= 20){
+        console.log("성인입니다.")
+    } else if (profile.age >= 8){
+        console.log("학생입니다.")
+    } else {
+        console.log("어린이입니다.")
+    }
+    // VM869:4 학생입니다.
+    // undefined
+    if (profile.age >= 20){
+        console.log("성인입니다.")
+    } else if ((profile.age <= 19) && (profile.age >= 8)){
+        console.log("학생입니다.")
+    } else {
+        console.log("어린이입니다.")
+    }
+    // VM909:4 학생입니다.
+    // undefined
+    if (profile.age >= 20){
+        console.log("성인입니다.")
+    } else if ((profile.age <= 19) && (profile.age >= 8)){
+        console.log("학생입니다.")
+    } else if(profile.age > 0) {
+        console.log("어린이입니다.")
+    } else {
+        console.log("잘못 입력하셨습니다.")
+    }
+    // 학생입니다.
+
+    
